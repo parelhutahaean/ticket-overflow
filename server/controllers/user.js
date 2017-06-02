fac02fddd0c6ff3d0670c5f908d8f8e5570a45f1
parelhutahaean <parel.hutahaean@gmail.com>
2017-06-02 14:24:15 +0700

    sf

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,17 +1,21 @@
 var User = require('../models/user')
+const jwt = require('jsonwebtoken');
 
 var methods = {}
 
 methods.insert = (req, res) => {
+  var token
   User.find({id:req.body.id}, (err, users) => {
     if(err) res.send({err})
     if(users.length == 0) {
       User.create(req.body, (err, user) => {
         if(err) res.send({err})
-        res.send(user)
+        token = jwt.sign(user._doc, 'SECRET');
+        res.send(token)
       })
     } else {
-      res.send(users[0])
+      token = jwt.sign(users[0]._doc, 'SECRET');
+      res.send(token)
     }
   })
 }
